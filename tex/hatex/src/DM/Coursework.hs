0b61912850a67e34316dafdd5a555969a0cb65c8
thymelous <tim.labushev@gmail.com>
2017-11-20 19:11:49 +0300

    [dm cw] pos minimal form

--- tex/hatex/src/DM/Coursework.hs
+++ tex/hatex/src/DM/Coursework.hs
@@ -54,3 +54,16 @@ reportTeX = do
                                        , And [X 1, Not (X 4), Not (X 5)]
                                        , And [X 1, Not (X 2), Not (X 3), Not (X 5)] ]) <> lnbk <> parbreak
     "Можно заметить, что в данном случае минимальное покрытие, полученное с помощью карт Карно, полностью совпадает с минимальным покрытием, полученным методом Квайна--Мак-Класки."
+    newpage
+    sectionstar "Определение МКНФ"
+    "Для определения МКНФ используется карта Карно с клетками, соответствующими наборам, на которых функция принимает нулевое значение, обозначенными нулем." <> lnbk <> parbreak
+    includegraphics [IGWidth $ Cm 12] "../src/DM/KarnaughMapPOS.pdf" <> lnbk <> parbreak
+    mt "C_{min} = \\set{\\text{00X0X, 0XX00, 000X0, 1X1XX, 1XX11, 11XX1, 11X1X}}" <> lnbk <> parbreak
+    mt "S^a = 3 + 3 + 4 + 2 + 3 + 3 + 3 = 21, \\quad S^b = 21 + 7 = 28" <> lnbk <> parbreak
+    "МКНФ: " <> (math . rendertex $ And [ Or [X 1, X 2, X 4]
+                                        , Or [X 1, X 4, X 5]
+                                        , Or [X 1, X 2, X 3, X 5]
+                                        , Or [Not (X 1), Not (X 3)]
+                                        , Or [Not (X 1), Not (X 4), Not (X 5)]
+                                        , Or [Not (X 1), Not (X 2), Not (X 5)]
+                                        , Or [Not (X 1), Not (X 2), Not (X 4)] ])
