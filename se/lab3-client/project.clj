6f0f0b98fcea53d4f304434ffc99b292b5f45940
thymelous <tim.labushev@gmail.com>
2017-12-04 13:06:37 +0300

    [se lab3] a simple data processor I need to tick all the boxes for this lab

---
+++ se/lab3-client/project.clj
@@ -0,0 +1,12 @@
+(defproject lab3-client "1.0.0"
+  :description "SE Lab 3 client-side script"
+  :url "http://github.com/thymelous/ifmo"
+  :license {:name "CC0 1.0"
+            :url "https://creativecommons.org/publicdomain/zero/1.0/legalcode"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/core.match "0.3.0-alpha5"]
+                 [clj-http "3.7.0"]
+                 [cheshire "5.8.0"]]
+  :main ^:skip-aot lab3-client.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
---
+++ se/lab3-client/src/lab3_client/core.clj
@@ -0,0 +1,51 @@
+(ns lab3-client.core
+  (:gen-class)
+  (:require [clojure.string :as str]
+            [clj-http.client :as client]
+            [cheshire.core :as json]))
+
+(declare prepare-report describe-action describe-transaction describe-conversation)
+
+(defn -main [& args]
+  (run! println (prepare-report)))
+
+(def host "http://localhost:8087/")
+
+(defn fetch-json [endpoint]
+  (-> (str host endpoint) client/get :body (json/parse-string true)))
+
+(defn prepare-report []
+  (let [actions (map describe-action
+                     (concat (fetch-json "actions/Незнайка")
+                             (fetch-json "actions/Козлик")))
+        transactions (map describe-transaction
+                        (distinct (concat (fetch-json "transactions/drawer/Незнайка")
+                                          (fetch-json "transactions/drawer/Незнайка")
+                                          (fetch-json "transactions/drawee/Незнайка")
+                                          (fetch-json "transactions/drawee/Незнайка"))))
+        conversations (map describe-conversation
+                         (distinct (concat (fetch-json "conversations/Незнайка")
+                                           (fetch-json "conversations/Козлик"))))]
+  (->> (concat actions transactions conversations) (sort-by :date) (map :description))))
+
+(defn parse-date [ds]
+  (.parse (java.text.SimpleDateFormat. "yyyy-MM-dd'T'HH:mm:ss") ds))
+
+(defn describe-transaction [t]
+  {:description (str (-> t :draweeLabel) " transferred "
+                     (-> t :amount) " currency units to "
+                     (-> t :drawerLabel))
+   :date (-> t :date parse-date)})
+
+(defn describe-conversation [c]
+  {:description (str "A conversation has been observed between "
+                      (str/join ", " (map (-> :participants :name) c))
+                     " and decoded to \"" (:recognizedContent c) "\"")
+   :date (-> c :date parse-date)})
+
+(defn describe-action [a]
+  {:description (str "A location change has been observed for "
+                     (-> a :actor :name)
+                     ": new location is " (:newLocation a)
+                     ", detected means of transporation: " (:means a))
+   :date (-> a :date parse-date)}) 
