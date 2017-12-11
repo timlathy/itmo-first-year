39400344558d490abc90d28a8d03b1f798bebd01
thymelous <tim.labushev@gmail.com>
2017-12-11 18:35:32 +0300

    [se lab3] db seeding...

--- se/lab3-client/src/lab3_client/seed.clj
+++ se/lab3-client/src/lab3_client/seed.clj
@@ -9,28 +9,75 @@
 
 (declare post-json)
 
+(defn create-person [person-name account-balance]
+  (-> {:name person-name}
+      (post-json "/people" (auth/jwt-big-brother)))
+  (-> {:ownerName person-name
+       :name (str "Individual " person-name)
+       :balance account-balance}
+      (post-json "/accounts" (auth/jwt-big-brother))))
+
+(defn create-conversation [person other-participants content date]
+  (-> {:participantNames other-participants
+       :content content
+       :date date}
+      (post-json "/conversations" (auth/jwt-device person))))
+
+(defn create-personal-transaction [drawee drawer amount date]
+  (-> {:personName drawer
+       :amount amount
+       :date date}
+      (post-json "/transactions" (auth/jwt-device drawee))))
+
+(defn create-business-transaction [drawee drawer amount date]
+  (-> {:businessName drawer
+       :amount amount
+       :date date}
+      (post-json "/transactions" (auth/jwt-device drawee))))
+
+(defn create-loc-change [person new-location means date]
+  (-> {:newLocation new-location
+       :means means
+       :date date}
+      (post-json "/actions/locationChanges" (auth/jwt-device person))))
+
+(defn create-business-and-owner [business-name owner-name owner-balance]
+  (create-person owner-name owner-balance)
+  (-> {:name business-name
+       :ownerName owner-name}
+      (post-json "/businesses" (auth/jwt-big-brother))))
+
 (defn post-data []
-  (-> {:name "Незнайка"} (post-json "/people"
-                                    (auth/jwt-big-brother)) :id)
-  (-> {:name "Козлик"} (post-json "/people"
-                                  (auth/jwt-big-brother)) :id)
-  (-> {:balance 10
-       :name "Individual Незнайка"
-       :ownerName "Незнайка"} (post-json "/accounts"
-                                         (auth/jwt-big-brother)))
-  (-> {:balance 10
-       :name "Individual Козлик"
-       :ownerName "Козлик"} (post-json "/accounts"
-                                       (auth/jwt-big-brother)))
-  (-> {:newLocation "Downtown"
-       :means "By foot"
-       :date "2028-06-12T08:17:30"} (post-json "/actions/locationChanges"
-                                                 (auth/jwt-device "Незнайка")))
-  (-> {:newLocation "Downtown"
-       :means "By foot"
-       :date "2028-06-12T08:18:30"} (post-json "/actions/locationChanges"
-                                                 (auth/jwt-device "Козлик")))
-  )
+  ; Main characters
+  (create-person "Незнайка" 10)
+  (create-person "Козлик" 10)
+  ; Rich people
+  (create-person "Богач Билли" 8192)
+  (create-person "Скряга Сэм" 16384)
+  (create-person "Толстосум Том" 32768)
+  ; A day in the life...
+  (create-loc-change "Незнайка" "Downtown" "By foot" "2028-06-12T08:17:40")
+  (create-loc-change "Козлик" "Downtown" "By foot" "2028-06-12T08:18:00")
+  ; First encounter
+  (create-conversation "Незнайка" '("Козлик" "Богач Билли") 
+                       "позвольте ... пожалуйста ... спасибо спасибо"
+                       "2028-06-12T09:32:00")
+  (create-conversation "Козлик" '("Незнайка" "Богач Билли")
+                       "какие тяжелые ... помочь ... вот сюда ... отлично"
+                       "2028-06-12T09:49:20")
+  (create-personal-transaction "Богач Билли" "Незнайка" 12 "2028-06-12T09:52:31")
+  ; Eating
+  (create-conversation "Незнайка" '("Козлик")
+                       "придется ... одноразовое питание ... лучше всего питаться вечером перед сном ... если проешь свои денежки днем или утром к вечеру ... проголодаешься и ночью не сможешь заснуть"
+                       "2028-06-12T19:19:20")
+  (create-business-and-owner "Умберто's" "Упитанный Умберто" 982)
+  (create-business-transaction "Незнайка" "Умберто's" 45 "2028-06-12T19:27:10")
+  ; Returning to a cheaper area
+  (create-loc-change "Козлик" "Outskirts" "By foot" "2028-06-12T21:01:00")
+  (create-loc-change "Незнайка" "Outskirts" "By foot" "2028-06-12T21:01:10")
+  ; Taking a "good" night's sleep
+  (create-business-and-owner "Ночлежка Грега" "Грязный Грег" 376)
+  (create-business-transaction "Незнайка" "Ночлежка Грега" 45 "2028-06-12T21:12:10"))
 
 ; Internal
 
