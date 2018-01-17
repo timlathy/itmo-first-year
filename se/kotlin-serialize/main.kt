c73ab3db7fbe5579c74b8b23189f0ec62e4422b0
thymelous <tim.labushev@gmail.com>
2018-01-17 09:11:28 +0300

    [se lab3/4] improve toy serializer with custom property names, etc.

--- se/kotlin-serialize/main.kt
+++ se/kotlin-serialize/main.kt
@@ -1,41 +1,61 @@
 import kotlin.reflect.full.memberProperties
+import kotlin.reflect.full.findAnnotation
 import kotlin.reflect.KVisibility
 import kotlin.reflect.KProperty1
+import kotlin.reflect.KClass
+
+import kotlin.annotation.Target
+import kotlin.annotation.AnnotationTarget
+
+@Target(AnnotationTarget.PROPERTY)
+annotation class JsonIgnore
+
+@Target(AnnotationTarget.PROPERTY)
+annotation class JsonRename(val name: String)
 
 fun main(_args: Array<String>) {
+  data class InnerObject(
+    val field: String)
+
   data class Person(
     val name: String,
     val age: Int,
+    @JsonRename("happy") val satisfiedWithLife: Boolean,
+    val innerObj: InnerObject = InnerObject("field value"),
+    @JsonIgnore val ignored: String = "ignored",
     private val hidden: String = "hidden")
   
-  val person = Person("Name", 20)
+  val person = Person("Robert'); DROP TABLE Students;--", 16, true)
   println(serialize(person))
 }
 
 fun serialize(obj: Any): String =
   obj.javaClass.kotlin.memberProperties
     .filter {
-      it.visibility == KVisibility.PUBLIC
+      it.visibility == KVisibility.PUBLIC &&
+        it.findAnnotation<JsonIgnore>() == null
     }
     .map {
-      val value =
-        it.get(obj)
+      val name = it.findAnnotation<JsonRename>()?.name ?: it.name
+      val value = it.get(obj)
       val serialized =
         if (value == null) "null"
         else serializePrimitive(it, value) ?: serialize(value).toString()
 
-      encode(it.name, serialized)
+      encode(name, serialized)
     }
     .joinToString(prefix = "{", postfix = "}") 
 
 fun encode(key: String, value: String): String =
-  "\"${key.escape()}\": \"${value.escape()}\""
+  "\"${key.escape()}\": $value"
 
 fun String.escape(): String =
   this.replace("\\", "\\\\").replace("\"", "\\\"")
 
-val PRIMITIVE_TYPES = listOf("kotlin.Int", "kotlin.String")
-
 fun <T, R> serializePrimitive(field: KProperty1<T, out R>, value: Any): String? =
-  if (field.returnType.toString() in PRIMITIVE_TYPES) value.toString()
-  else null
+  when (field.returnType.toString()) {
+    "kotlin.Int" -> value.toString()
+    "kotlin.String" -> "\"${(value as String).escape()}\""
+    "kotlin.Boolean" -> value.toString()
+    else -> null
+  }
