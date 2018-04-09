3b3d5de8678bf1aeb5e8d40038e7911b5fa801f9
thymelous <tim.labushev@gmail.com>
2018-04-09 15:24:01 +0300

    [se lab6] import command

--- se/lab6-client/src/main/kotlin/ru/ifmo/se/lab6/client/CustomCommands.kt
+++ se/lab6-client/src/main/kotlin/ru/ifmo/se/lab6/client/CustomCommands.kt
@@ -1,16 +1,18 @@
 package ru.ifmo.se.lab6.client
 
+import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
 import java.io.File
 import java.io.IOException
 import java.io.PrintWriter
+import java.util.*
 
 class CustomCommands {
   companion object {
     private val mapper = ObjectMapper().apply { findAndRegisterModules() }
 
     fun list(): List<ServerCommand> =
-      listOf(SaveCommand(mapper))
+      listOf(SaveCommand(mapper), ImportCommand(mapper))
   }
 
   /**
@@ -36,6 +38,39 @@ class CustomCommands {
 
       return CommandRunner.CommandResult("The queue has been successfully saved.",
         CommandRunner.CommandStatus.SUCCESS)
+    }
+  }
+
+  /**
+   * Adds all elements from the specified file to the queue.
+   */
+  class ImportCommand(private val mapper: ObjectMapper): ServerCommand("import", ArgumentType.FILE_PATH) {
+    override fun exec(conn: ServerConnection, arg: String?, name: String): CommandRunner.CommandResult {
+      val target = File(arg)
+      try {
+        target.createNewFile()
+        if (!target.canRead()) throw IOException("The specified file is not readable.")
+      } catch (e: IOException) {
+        return CommandRunner.CommandResult(e.message ?:
+          "An error has occurred while reading the specified file.", CommandRunner.CommandStatus.ERROR)
+      }
+
+      val json = StringBuilder().apply {
+        Scanner(target).let { scan ->
+          while (scan.hasNext()) append(scan.nextLine())
+          scan.close()
+        }
+        if (isEmpty()) append("[]")
+      }.toString()
+
+      val queue: Array<JsonNode> = with(mapper) {
+        readValue(json, typeFactory.constructArrayType(JsonNode::class.java))
+      }
+      val result = queue.map(mapper::writeValueAsString).joinToString("\n") { element ->
+        super.exec(conn, element, "add").message
+      }
+
+      return CommandRunner.CommandResult(result, CommandRunner.CommandStatus.SUCCESS)
     }
   }
 }
