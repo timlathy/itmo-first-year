5731673a9fda6afd633802b5da1cb5100830a4e8
thymelous <tim.labushev@gmail.com>
2018-04-09 17:50:04 +0300

    [se lab6] allow client to listen to ports other than 8080

--- se/lab6-client/src/main/kotlin/ru/ifmo/se/lab6/client/Application.kt
+++ se/lab6-client/src/main/kotlin/ru/ifmo/se/lab6/client/Application.kt
@@ -5,8 +5,10 @@ import java.net.InetSocketAddress
 import kotlin.system.exitProcess
 
 fun main(args: Array<String>) {
+  val port = args.firstOrNull()?.toInt() ?: 8080
+
   try {
-    val conn = ServerConnection(InetSocketAddress(8080))
+    val conn = ServerConnection(InetSocketAddress(port))
     val runner = initRunnerWithConnection(conn, CustomCommands.list())
     Repl(runner).loop()
   }
