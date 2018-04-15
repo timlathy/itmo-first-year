a746e6f48df97f24ffedd3a1e398dd5b5aad684d
thymelous <tim.labushev@gmail.com>
2018-04-15 23:02:21 +0300

    [db lab2] assignment updates

--- db/lab2tasks.sql
+++ db/lab2tasks.sql
@@ -1,34 +1,33 @@
 -- Task 1
 
-create temp view romantic_relationships as
-  select id, array_agg(member_id order by member_id) as partner_ids from relationships
+with romantic_relationships as (
+  select id, array_agg(member_id order by member_id) as partner_ids
+  from relationships
   inner join crew_members_relationships on id = relationship_id
   where kind = 'romance'
-  group by id;
-
-create or replace function duo_observations(used_tool_name text)
-  returns table (observer_ids integer[]) as
-  $$
-    select array_agg(observer_id order by observer_id) from scientific_observations
-      where equipment_id = (select id from observation_tools where name = $1)
-      group by observed_on having count(*) = 2
-  $$
-  language sql;
-
+  group by id
+),
+duo_telescope_observations as (
+  select array_agg(observer_id order by observer_id)
+  from scientific_observations
+  where equipment_id = (select id from observation_tools where name = 'Telescope')
+  group by observed_on
+  having count(*) = 2
+)
 select id relationship_id, partner_ids
-  from romantic_relationships
-  where partner_ids in (select duo_observations('Telescope'));
+from romantic_relationships
+where partner_ids in (select * from duo_telescope_observations);
 
 -- Task 2
 
 create or replace function observation_log(starting_from_id integer)
-  returns setof text as
-  $$
+  returns setof text as $$
     declare
       current_item record;
     begin
       select into current_item * from scientific_observations
         where id = starting_from_id;
+
       loop
         return next format(
             '%s - %s - %s - %s',
@@ -36,8 +35,10 @@ create or replace function observation_log(starting_from_id integer)
             (select name from observation_tools where id = current_item.equipment_id),
             current_item.details,
             current_item.observed_on);
+
         select into current_item * from scientific_observations
           where base_observation_id = current_item.id;
+
         if current_item is null then
           return;
         end if;
