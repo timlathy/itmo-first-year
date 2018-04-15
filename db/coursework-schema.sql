36f3fad4a3a640c90846a99e6b79c7fdb4dc34f0
thymelous <tim.labushev@gmail.com>
2018-04-15 21:21:30 +0300

    [db cw] major schema refactor

--- db/coursework-schema.sql
+++ db/coursework-schema.sql
@@ -1,3 +1,4 @@
+begin;
 create type exam_mark as enum (
   'passed', 'not passed'
 );
@@ -26,7 +27,7 @@ create type delivery_content as enum (
   '1st class mail', '2nd class mail', 'parcel', 'periodical'
 );
 
-create type club_category as enum (
+create type club_categories as enum (
   'sport', 'art', 'science', 'other'
 );
 
@@ -35,22 +36,20 @@ create type week_day as enum (
   'Wednesday', 'Thursday', 'Friday', 'Saturday'
 );
 
+create type spell_type as enum (
+  'conjuration', 'charm',
+  'healing spell', 'curse', 'transfiguration'
+);
+
 create table houses (
-  id      bigserial         not null
-    constraint faculties_pkey
-    primary key,
+  id      bigserial primary key,
   name    text              not null,
   dean_id bigint            not null,
   values  personal_value [] not null
 );
 
-create unique index faculties_id_uindex
-  on houses (id);
-
 create table people (
-  id             bigserial      not null
-    constraint people_pkey
-    primary key,
+  id             bigserial primary key,
   full_name      text           not null,
   birth_date     timestamp      not null,
   bio            text,
@@ -61,197 +60,98 @@ create table people (
   constraint birth_death_date_order check (birth_date < death_date)
 );
 
-create unique index people_id_uindex
-  on people (id);
-
 alter table houses
-  add constraint faculties_people_dean_id_fk
-foreign key (dean_id) references people;
-
-create table events
-(
-  id         bigserial not null
-    constraint events_pkey
-    primary key,
-  name       text,
-  ended_on   timestamp not null,
-  started_on timestamp not null,
-
-  constraint start_end_date_order check (started_on <= ended_on)
-);
-
-create unique index events_id_uindex
-  on events (id);
-
-create table event_participation
-(
-  id        bigserial not null
-    constraint event_participation_pkey
-    primary key,
-  event_id  bigint    not null
-    constraint event_participation_event_id_fk
-    references events,
-  person_id bigint    not null
-    constraint event_participation_person_id_fk
-    references people,
-  positive  boolean   not null,
-  score     integer   not null,
-  date      timestamp not null,
-
-  constraint positive_score check (score > 0)
-);
-
-create unique index event_participation_id_uindex
-  on event_participation (id);
+  add foreign key (dean_id) references people;
 
 create table creatures
 (
   generic_name       text                    not null,
   discovered_on      timestamp               not null,
-  id                 bigserial               not null
-    constraint creatures_id_pk
-    primary key,
+  id                 bigserial primary key,
   mom_classification creature_classification not null
 );
 
-create unique index creatures_id_uindex
-  on creatures (id);
-
 create table books
 (
-  id                  bigserial not null
-    constraint documents_pkey
-    primary key,
+  id                  bigserial primary key,
   title               text      not null,
   added_on            timestamp not null,
   requires_permission boolean   not null
 );
 
-create unique index documents_id_uindex
-  on books (id);
-
 create table creature_books
 (
-  creature_id bigint not null
-    constraint creature_documentation_creature_id_fk
-    references creatures,
-  book_id     bigint
-    constraint creature_documentation_book_id_fk
-    references books
+  creature_id bigint not null references creatures,
+  book_id     bigint not null references books
+);
+
+create table student_clubs (
+  id           serial primary key,
+  name         text            not null,
+  president_id integer         not null,
+  category     club_categories not null
 );
 
 create table student_profiles
 (
-  id             bigserial not null
-    constraint student_profiles_pkey
-    primary key,
-  person_id      bigint    not null
-    constraint student_profiles_person_id_fk
-    references people,
-  study_plan_id  bigint    not null,
-  dormitory_room integer   not null
+  id             bigserial primary key,
+  person_id      bigint references people,
+  club_id        bigint references student_clubs,
+  study_plan_id  bigint  not null,
+  dormitory_room integer not null
 );
 
-create unique index student_profiles_id_uindex
-  on student_profiles (id);
-
-create unique index student_profiles_person_id_uindex
-  on student_profiles (person_id);
+alter table student_clubs
+  add foreign key (president_id) references student_profiles;
 
 create table study_plans
 (
-  id            bigserial not null
-    constraint study_plans_pkey
-    primary key,
-  house_id      bigint    not null
-    constraint study_plans_house_id_fk
-    references houses,
-  academic_year integer   not null,
+  id            bigserial primary key,
+  house_id      bigint  not null references houses,
+  academic_year integer not null,
 
   constraint academic_year_validity check (academic_year between 1 and 7)
 );
 
-create unique index study_plans_id_uindex
-  on study_plans (id);
-
 alter table student_profiles
-  add constraint student_profiles_study_plan_id_fk
-foreign key (study_plan_id) references study_plans;
+  add foreign key (study_plan_id) references study_plans;
 
 create table subjects
 (
-  id            bigserial not null
-    constraint subjects_pkey
-    primary key,
-  name          text      not null,
-  study_plan_id bigint    not null
-    constraint subjects_study_plan_id_fk
-    references study_plans,
-  teacher_id    bigint    not null
-    constraint subjects_people_teacher_id_fk
-    references people
+  id            bigserial primary key,
+  name          text not null,
+  study_plan_id bigint references study_plans,
+  teacher_id    bigint references people
 );
-
-create unique index subjects_id_uindex
-  on subjects (id);
 
 create table exam_results
 (
-  id                 bigserial                                   not null
-    constraint exam_results_id_pk
-    primary key,
-  subject_id         bigint                                      not null
-    constraint exam_results_subject_id_fk
-    references subjects,
-  student_profile_id bigint
-    constraint exam_results_student_profile_id_fk
-    references student_profiles,
-  mark               exam_mark default 'not passed' :: exam_mark not null
+  id                 bigserial primary key,
+  subject_id         bigint    not null references subjects,
+  student_profile_id bigint    not null references student_profiles,
+  mark               exam_mark not null default 'not passed' :: exam_mark
 );
-
-create unique index exam_results_id_uindex
-  on exam_results (id);
 
 create table book_lendings
 (
-  id              bigserial not null
-    constraint book_lendings_pkey
-    primary key,
-  book_id         bigint    not null
-    constraint book_lendings_book_id_fk
-    references books,
-  lendee_id       bigint    not null
-    constraint book_lendings_lendee_id_fk
-    references people,
-  permitted_by_id bigint
-    constraint book_lendings_permitted_by_id_fk
-    references people,
+  id              bigserial primary key,
+  book_id         bigint    not null references books,
+  lendee_id       bigint    not null references people,
+  permitted_by_id bigint references people,
   checked_out_on  timestamp not null,
   checked_in_on   timestamp,
 
   constraint checked_out_checked_in_date_order check (checked_out_on < book_lendings.checked_in_on)
 );
 
-create unique index book_lendings_id_uindex
-  on book_lendings (id);
-
 create table creature_domestications
 (
-  id                 bigserial not null
-    constraint creature_domestications_pkey
-    primary key,
-  creature_id        bigint    not null
-    constraint creature_domestications_creature_id_fk
-    references creatures,
-  domesticated_by_id bigint    not null
-    constraint creature_domestications_domesticated_by_id_fk
-    references people,
+  id                 bigserial primary key,
+  creature_id        bigint    not null references creatures,
+  domesticated_by_id bigint    not null references people,
   domesticated_on    timestamp not null,
   name_given         text      not null
 );
-
-create unique index creature_domestications_id_uindex
-  on creature_domestications (id);
 
 create table delivery_owls (
   name     text not null,
@@ -262,8 +162,8 @@ create table delivery_owls (
 
 create table delivery_owl_flights (
   id               serial,
-  sender_id        integer references people (id),
-  owl_id           integer references delivery_owls (id),
+  sender_id        integer          not null references people,
+  owl_id           integer          not null references delivery_owls,
   dest_coordinates coordinates      not null,
   contents_type    delivery_content not null,
   departed_on      timestamp        not null,
@@ -272,28 +172,61 @@ create table delivery_owl_flights (
 
 create table delivery_owl_repair_jobs (
   id                  serial,
-  owl_id              integer references delivery_owls (id),
-  tech_ops_maneger_id integer references people (id),
+  owl_id              integer   not null references delivery_owls,
+  tech_ops_manager_id integer   not null references people,
   cause               text      not null,
   began_on            timestamp not null,
   finished_on         timestamp
 );
 
-create table student_clubs (
-  id           serial primary key,
-  president_id integer references people (id),
-  name         text            not null,
-  category     club_categories not null
+create table classroom_bookings (
+  id              serial primary key,
+  subject_id      integer  not null references subjects,
+  student_club_id integer  not null references student_clubs,
+  room_number     integer  not null,
+  week_day        week_day not null,
+  occupied_from   time     not null,
+  occupied_to     time     not null,
+
+  constraint booked_by_single_entity check
+  (((subject_id is not null) :: integer + (student_club_id is not null) :: integer) = 1)
 );
 
-create table classroom_bookings (
-  id                 serial primary key,
-  subject_id         integer references subjects (id),
-  student_profile_id integer references student_profiles (id),
-  room_number        integer  not null,
-  week_day           week_day not null,
-  occupied_from      time     not null,
-  occupied_to        time     not null
+create table events
+(
+  id         bigserial primary key,
+  name       text,
+  ended_on   timestamp not null,
+  started_on timestamp not null,
+
+  constraint start_end_date_order check (started_on <= ended_on)
+);
+
+create table event_participations
+(
+  id                 bigserial primary key,
+  event_id           bigint    not null references events,
+  student_profile_id bigint    not null references student_profiles,
+  is_positive        boolean   not null,
+  score              integer   not null,
+  date               timestamp not null,
+
+  constraint positive_score check (score > 0)
+);
+
+create table spells (
+  id              serial primary key,
+  counterspell_id integer references spells,
+  creator_id      integer references people,
+  name            text       not null,
+  description     text       not null,
+  type            spell_type not null,
+  is_forbidden    boolean
+);
+
+create table spell_books (
+  spell_id integer not null references spells,
+  book_id  integer not null references books
 );
 
 -- check_classroom_availability
@@ -306,9 +239,9 @@ begin
   is_occupied := (select exists(select
                                 from classroom_bookings
                                 where room_number = new.room_number
-                                      and week_day = new.weekday,
-                                and ((new.occupied_from >= occupied_from and new.occupied_from <= occupied_to)
-                                     or (new.occupied_to >= occupied_from and new.occupied_to <= occupied_to))
+                                      and week_day = new.week_day
+                                      and ((new.occupied_from >= occupied_from and new.occupied_from <= occupied_to)
+                                           or (new.occupied_to >= occupied_from and new.occupied_to <= occupied_to))
   ));
 
   if is_occupied
@@ -398,3 +331,27 @@ create trigger validate_owl_availability
   before insert
   on delivery_owl_flights
   for each row execute procedure check_owl_availability();
+
+-- set_permission_requirement_on_forbidden_spell_books
+
+create function set_permission_requirement_on_forbidden_spell_books()
+  returns trigger as $$
+declare
+  is_forbidden boolean;
+begin
+  is_forbidden := (select is_forbidden
+                   from spells
+                   where id = new.spell_id);
+  if is_forbidden
+  then update books set requires_permission = true where id = new.book_id;
+  end if;
+end;
+$$
+language plpgsql;
+
+create trigger enforce_spell_book_permission_policy
+  after insert
+  on spell_books
+  for each row execute procedure set_permission_requirement_on_forbidden_spell_books();
+
+commit;
